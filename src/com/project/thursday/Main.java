afeb8170a308aab6351cd0476442a4fefa2ca42e
Conor Taggart <conort@kainos.com>
2016-10-13 13:46:11 +0100

    adding insert from employees to table

--- src/com/project/thursday/Main.java
+++ src/com/project/thursday/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main_menu.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Employee Management System");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
     }
 
