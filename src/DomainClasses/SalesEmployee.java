8dd033e77bcaa8b236d5ad8b28a56c176b910575
Eoghan Bradley <eoghanb@prada.local>
2016-10-13 14:44:42 +0100

    created sales employee  class

---
+++ src/DomainClasses/SalesEmployee.java
@@ -0,0 +1,29 @@
+package DomainClasses;
+
+public class SalesEmployee extends Employee {
+
+    private int id;
+    private float commission_rate;
+    private double total_sales;
+
+    //constructors
+    public void SalesEmployee(){
+
+    }
+    
+    public float getCommission_rate() {
+        return commission_rate;
+    }
+
+    public void setCommission_rate(float commission_rate) {
+        this.commission_rate = commission_rate;
+    }
+
+    public double getTotal_sales() {
+        return total_sales;
+    }
+
+    public void setTotal_sales(double total_sales) {
+        this.total_sales = total_sales;
+    }
+}
