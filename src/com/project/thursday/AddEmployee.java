14da735e9d98cbc5028c066e1fa5deccdc6bd599
Conor Taggart <conort@kainos.com>
2016-10-14 10:37:35 +0100

    fixed alert dialog generation

--- src/com/project/thursday/AddEmployee.java
+++ src/com/project/thursday/AddEmployee.java
@@ -2,14 +2,14 @@ package com.project.thursday;
 
 import DomainClasses.Bu;
 import DomainClasses.Employee;
+import DomainClasses.SalesEmployee;
 import exceptions.ConnectionException;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import persistance.DepartmentManager;
 import persistance.EmployeeManager;
@@ -33,6 +33,12 @@ public class AddEmployee implements Initializable {
     public TextField accoutText;
     public TextField salaryText;
     public ComboBox<Bu> buCB;
+    public RadioButton empRB;
+    public RadioButton salesEmpRB;
+    public TextField commText;
+    public TextField totSalesText;
+    public Label totSaleLabel;
+    public Label comLabel;
 
     @FXML
     private Button addButton, cancelButton;
@@ -42,13 +48,28 @@ public class AddEmployee implements Initializable {
         try {
             buCB.getItems().setAll(new DepartmentManager().getAllBU(false));
             buCB.getSelectionModel().select(0);
-        } catch (ConnectionException ex){
+            ToggleGroup tg = new ToggleGroup();
+            tg.getToggles().add(empRB);
+            tg.getToggles().add(salesEmpRB);
+
+            empRB.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                @Override
+                public void changed(ObservableValue<? extends Boolean> obs, Boolean wasPreviouslySelected, Boolean isNowSelected) {
+                    commText.setVisible(!isNowSelected);
+                    comLabel.setVisible(!isNowSelected);
+                    totSaleLabel.setVisible(!isNowSelected);
+                    totSalesText.setVisible(!isNowSelected);
+                }
+            });
+            empRB.setSelected(true);
+        } catch (ConnectionException ex) {
             ex.printStackTrace();
         }
     }
 
     public void add(ActionEvent actionEvent) {
         try {
+            if(!salesEmpRB.isSelected()){
             Employee employee = new Employee();
             employee.setFirst_name(fNameText.getText());
             employee.setLast_name(lNameText.getText());
@@ -60,28 +81,46 @@ public class AddEmployee implements Initializable {
             employee.setBank_account(accoutText.getText());
             employee.setSalary(Double.valueOf((salaryText).getText()));
             new EmployeeManager().addEmployee(employee, buCB.getValue());
-            Stage stage = (Stage) addButton.getScene().getWindow();
-            stage.close();
+            } else {
+                SalesEmployee employee = new SalesEmployee();
+                employee.setFirst_name(fNameText.getText());
+                employee.setLast_name(lNameText.getText());
+                employee.setAddress_1(add1Text.getText());
+                employee.setAddress_2(add2Text.getText());
+                employee.setCity(cityText.getText());
+                employee.setPostcode(pcodeText.getText());
+                employee.setNational_insurance(niText.getText());
+                employee.setBank_account(accoutText.getText());
+                employee.setSalary(Double.valueOf((salaryText).getText()));
+                employee.setCommission_rate(Float.valueOf(commText.getText()));
+                employee.setTotal_sales(Double.valueOf(totSalesText.getText()));
+                new EmployeeManager().addSalesEmployee(employee, buCB.getValue());
+            }
+            close();
         } catch (IllegalArgumentException argEx) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Employee Details Error");
-            alert.setContentText(argEx.getMessage());
+            createAlert("Employee Details Error", argEx.getMessage());
             argEx.printStackTrace();
-        } catch (ConnectionException ex){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Connection Error");
-            alert.setContentText(ex.getMessage());
+        } catch (ConnectionException ex) {
+            createAlert("Connection Error", ex.getMessage());
             ex.printStackTrace();
-        } catch (SQLException sql){
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("SQL Error");
-            alert.setContentText(sql.getMessage());
+        } catch (SQLException sql) {
+            createAlert("SQL Error",sql.getMessage());
             sql.printStackTrace();
         }
     }
 
+    public void createAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 
     public void cancel(ActionEvent actionEvent) {
+        close();
+    }
+
+    public void close() {
         Stage stage = (Stage) addButton.getScene().getWindow();
         stage.close();
     }
