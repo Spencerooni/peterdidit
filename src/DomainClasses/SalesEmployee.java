ad9b42798e80cc59cd7fd9104ee11c2f44bd2055
Peter Bonar <peterbo@roberto.local>
2016-10-14 14:28:31 +0100

    Added error message

--- src/DomainClasses/SalesEmployee.java
+++ src/DomainClasses/SalesEmployee.java
@@ -14,16 +14,22 @@ public class SalesEmployee extends Employee {
         return commission_rate;
     }
 
-    public void setCommission_rate(float commission_rate) {
-        this.commission_rate = commission_rate;
+    public void setCommission_rate(float commission_rate) throws IllegalArgumentException {
+
+        if(commission_rate < 0){
+            throw new IllegalArgumentException("Please enter a Commission Rate greater than 0");
+        } else this.commission_rate = commission_rate;
     }
 
     public double getTotal_sales() {
         return total_sales;
     }
 
-    public void setTotal_sales(double total_sales) {
-        this.total_sales = total_sales;
+    public void setTotal_sales(double total_sales) throws IllegalArgumentException {
+
+        if(total_sales < 0){
+            throw new IllegalArgumentException("Please enter a Total Sales greater than 0");
+        } else this.total_sales = total_sales;
     }
 
     @Override
