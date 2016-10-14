b0ff9bdf12342b94f7d583f54e017721ded89167
Eoghan Bradley <eoghanb@prada.local>
2016-10-14 10:45:10 +0100

    Created EmployeePay domain class for report salaryreport generation

---
+++ src/DomainClasses/EmployeePay.java
@@ -0,0 +1,17 @@
+package DomainClasses;
+
+public class EmployeePay {
+
+    private String employeeName;
+    private String finalSalary;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public String getFinalSalary() {
+        return finalSalary;
+    }
+
+
+}
